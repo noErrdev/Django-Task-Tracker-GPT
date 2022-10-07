f6dcacf77ea47e1946ac1048d51f953a10ba954e
noErrdev <dbongo0701@gmail.com>
2022-10-07 00:17:38 -0700

    feat: add white noise for static file serving

--- main/settings.py
+++ main/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,7 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'dist/assets'),
