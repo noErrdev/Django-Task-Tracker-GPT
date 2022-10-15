52001eeacd40df68af5f5231fcddfbfba3c92025
noErrdev <dbongo0701@gmail.com>
2022-10-14 23:54:06 -0700

    chore: update secret key

--- main/settings.py
+++ main/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-)o_#9)hgow+k0w4%hmerf(g&wx4ob2npa&0t4la^su7f4ih8k+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
